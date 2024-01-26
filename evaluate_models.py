172a44780be637af26e72c2306fe02a17154161c
jung0072 <jung0072@algonquinlive.com>
2024-01-26 15:41:59 -0500

    list mmpretrain possible models

--- evaluate_models.py
+++ evaluate_models.py
@@ -1,17 +1,20 @@
 from mmpretrain import get_model
+from mmpretrain import list_models
 import os
 
-model_resnet = get_model("custom_resnet", pretrained=True)
-model_mobile = get_model("custom_mobilenet-v3-small", pretrained=True)
+list_models()
 
-test_image_path = "./data/flower_dataset/test"
+# model_resnet = get_model("custom_resnet", pretrained=True)
+# model_mobile = get_model("custom_mobilenet-v3-small", pretrained=True)
 
-for label in os.listdir(test_image_path):
-    label_path = os.path.join(test_image_path, label)
-    for image in os.listdir(label_path):
-        image_path = os.path.join(label_path, image)
-        result_resnet = model_resnet.classify(image_path)
-        print(f"GT Lable: {label}, Prediction: {result_resnet}")
-        result_mobile = model_mobile.classify(image_path)
-        print(f"GT Lable: {label}, Prediction: {result_mobile}")
-        break
+# test_image_path = "./data/flower_dataset/test"
+
+# for label in os.listdir(test_image_path):
+#     label_path = os.path.join(test_image_path, label)
+#     for image in os.listdir(label_path):
+#         image_path = os.path.join(label_path, image)
+#         result_resnet = model_resnet.classify(image_path)
+#         print(f"GT Lable: {label}, Prediction: {result_resnet}")
+#         result_mobile = model_mobile.classify(image_path)
+#         print(f"GT Lable: {label}, Prediction: {result_mobile}")
+#         break
